bacbacbc39e297ce567eb8adeff4ffd977d9d5d3
Daniel-Kigozi <kdanalex69@gmail.com>
2023-01-19 14:19:46 +0300

    fixed bug on multiplication

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -29,17 +29,17 @@ class Calculator extends React.Component {
     return (
       <div className="wrapper">
 
-        <input type="text" className="operator display" id="calc-screen" placeholder="0" value={val} />
+        <input type="text" className="operator display" id="calc-screen" placeholder="0" value={val} readOnly />
 
         <button type="button" className="operator all-clear" name="AC" onClick={this.handleClick}>AC</button>
         <button type="button" className="operator Plus-minus operations" name="+/-" onClick={this.handleClick}>+/-</button>
         <button type="button" className="operator" name="%" onClick={this.handleClick}>%</button>
-        <button type="button" className="operator operation" name="+" onClick={this.handleClick}>+</button>
+        <button type="button" className="operator operation" name="÷" onClick={this.handleClick}>&divide;</button>
 
         <button type="button" className="operator number" name="7" onClick={this.handleClick}>7</button>
         <button type="button" className="operator number" name="8" onClick={this.handleClick}>8</button>
         <button type="button" className="operator number" name="9" onClick={this.handleClick}>9</button>
-        <button type="button" className="operator operation" name="x" onClick={this.handleClick}>&times;</button>
+        <button type="button" className="operator operation" name="x" onClick={this.handleClick}>x</button>
 
         <button type="button" className="operator number" name="4" onClick={this.handleClick}>4</button>
         <button type="button" className="operator number" name="5" onClick={this.handleClick}>5</button>
@@ -49,7 +49,7 @@ class Calculator extends React.Component {
         <button type="button" className="operator number" name="1" onClick={this.handleClick}>1</button>
         <button type="button" className="operator number" name="2" onClick={this.handleClick}>2</button>
         <button type="button" className="operator number" name="3" onClick={this.handleClick}>3</button>
-        <button type="button" className="operator operation" name="÷" onClick={this.handleClick}>&divide;</button>
+        <button type="button" className="operator operation" name="+" onClick={this.handleClick}>+</button>
 
         <button type="button" className="operator number zero" name="0" onClick={this.handleClick}>0</button>
         <button type="button" className="operator number" name="." onClick={this.handleClick}>.</button>
