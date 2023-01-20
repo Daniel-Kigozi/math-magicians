1ed1ebed6adc5efba4a4a56cfb416f545a387b7d
Daniel-Kigozi <kdanalex69@gmail.com>
2023-01-20 08:21:48 +0300

    refactor to function based

--- src/App.js
+++ src/App.js
@@ -2,19 +2,10 @@ import React from 'react';
 import Calculator from './components/calculator';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Calculator />
+  </div>
+);
 
 export default App;
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,62 +1,55 @@
 import React from 'react';
 import './calculator.css';
-import calculate from '../logic/calculate';
 import '../logic/operate';
+import PropTypes from 'prop-types';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: 0,
-    };
-    this.handleClick = this.handleClick.bind(this);
+const Calculator = (props) => {
+  const { calc, handleClick } = props;
+  const { total, next } = calc;
+
+  let val = 0;
+  if (next) {
+    val = next;
+  } else if (total) {
+    val = total;
   }
 
-  handleClick(e) {
-    this.setState((previousState) => calculate(previousState, e.target.innerText));
-  }
+  return (
+    <div className="wrapper">
 
-  render() {
-    let val = 0;
-    const { total, next } = this.state;
-    if (next) {
-      val = next;
-    } else if (total) {
-      val = total;
-    }
+      <input type="text" className="operator display" id="calc-screen" placeholder="0" value={val} readOnly />
 
-    return (
-      <div className="wrapper">
+      <button type="button" className="operator all-clear" name="AC" onClick={handleClick}>AC</button>
+      <button type="button" className="operator Plus-minus operations" name="+/-" onClick={handleClick}>+/-</button>
+      <button type="button" className="operator" name="%" onClick={handleClick}>%</button>
+      <button type="button" className="operator operation" name="÷" onClick={handleClick}>&divide;</button>
 
-        <input type="text" className="operator display" id="calc-screen" placeholder="0" value={val} readOnly />
+      <button type="button" className="operator number" name="7" onClick={handleClick}>7</button>
+      <button type="button" className="operator number" name="8" onClick={handleClick}>8</button>
+      <button type="button" className="operator number" name="9" onClick={handleClick}>9</button>
+      <button type="button" className="operator operation" name="x" onClick={handleClick}>x</button>
 
-        <button type="button" className="operator all-clear" name="AC" onClick={this.handleClick}>AC</button>
-        <button type="button" className="operator Plus-minus operations" name="+/-" onClick={this.handleClick}>+/-</button>
-        <button type="button" className="operator" name="%" onClick={this.handleClick}>%</button>
-        <button type="button" className="operator operation" name="÷" onClick={this.handleClick}>&divide;</button>
+      <button type="button" className="operator number" name="4" onClick={handleClick}>4</button>
+      <button type="button" className="operator number" name="5" onClick={handleClick}>5</button>
+      <button type="button" className="operator number" name="6" onClick={handleClick}>6</button>
+      <button type="button" className="operator operation" name="-" onClick={handleClick}>-</button>
 
-        <button type="button" className="operator number" name="7" onClick={this.handleClick}>7</button>
-        <button type="button" className="operator number" name="8" onClick={this.handleClick}>8</button>
-        <button type="button" className="operator number" name="9" onClick={this.handleClick}>9</button>
-        <button type="button" className="operator operation" name="x" onClick={this.handleClick}>x</button>
+      <button type="button" className="operator number" name="1" onClick={handleClick}>1</button>
+      <button type="button" className="operator number" name="2" onClick={handleClick}>2</button>
+      <button type="button" className="operator number" name="3" onClick={handleClick}>3</button>
+      <button type="button" className="operator operation" name="+" onClick={handleClick}>+</button>
 
-        <button type="button" className="operator number" name="4" onClick={this.handleClick}>4</button>
-        <button type="button" className="operator number" name="5" onClick={this.handleClick}>5</button>
-        <button type="button" className="operator number" name="6" onClick={this.handleClick}>6</button>
-        <button type="button" className="operator operation" name="-" onClick={this.handleClick}>-</button>
+      <button type="button" className="operator number zero" name="0" onClick={handleClick}>0</button>
+      <button type="button" className="operator number" name="." onClick={handleClick}>.</button>
+      <button type="button" className="operator operation" name="=" onClick={handleClick}>=</button>
 
-        <button type="button" className="operator number" name="1" onClick={this.handleClick}>1</button>
-        <button type="button" className="operator number" name="2" onClick={this.handleClick}>2</button>
-        <button type="button" className="operator number" name="3" onClick={this.handleClick}>3</button>
-        <button type="button" className="operator operation" name="+" onClick={this.handleClick}>+</button>
+    </div>
+  );
+};
 
-        <button type="button" className="operator number zero" name="0" onClick={this.handleClick}>0</button>
-        <button type="button" className="operator number" name="." onClick={this.handleClick}>.</button>
-        <button type="button" className="operator operation" name="=" onClick={this.handleClick}>=</button>
+Calculator.propTypes = {
+  calc: PropTypes.instanceOf(Object).isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
 
-      </div>
-    );
-  }
-}
 export default Calculator;
